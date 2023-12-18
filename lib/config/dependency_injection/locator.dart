69afc1bdb9d03bc07891598502cf292a1fdb7fb1
Sergi Portero <serporterolo01@gmail.com>
2023-12-18 13:23:20 +0100

    ChangedFactory to Singleton BeerProvider

--- lib/config/dependency_injection/locator.dart
+++ lib/config/dependency_injection/locator.dart
@@ -2,14 +2,13 @@ import 'package:get_it/get_it.dart';
 import 'package:memorandum_pt/config/config.dart';
 import 'package:memorandum_pt/data/data.dart';
 import 'package:memorandum_pt/data/data_provider/beer/beer_provider.dart';
-import 'package:memorandum_pt/data/data_provider/beer/beer_provider_interface.dart';
 
 GetIt locator = GetIt.instance;
 
 void setupLocator() {
   locator
       .registerLazySingleton<NavigationHandler>(() => MainNavigationHandler());
-  locator.registerFactory<BeerProvider>(() => BeerProvider());
+  locator.registerSingleton<BeerProvider>(BeerProvider());
   locator.registerLazySingleton<Repository>(
     () => Repository(beerProvider: locator<BeerProvider>()),
   );
